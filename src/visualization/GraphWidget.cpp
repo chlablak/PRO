f9ef7885593b9ba717e7695db5a20fa6721423a9
Damien Rochat <rochat.damien@gmail.com>
2016-05-22 20:28:08 +0200

    Implémentation du GraphSceneManager pour pouvoir réutiliser la même instance d'une scéne

--- src/visualization/GraphWidget.cpp
+++ src/visualization/GraphWidget.cpp
@@ -1,17 +1,15 @@
-#include <QDebug>
 #include <QVBoxLayout>
 
 #include "../graph/graphs/IGraph.h"
 
 #include "GraphWidget.h"
-#include "view/GraphView.h"
-#include "view/GraphScene.h"
+#include "view/GraphSceneManager.h"
 
 GraphWidget::GraphWidget(const IGraph *graph, QWidget *parent)
     : QWidget(parent)
 {
     view = new GraphView();
-    scene = new GraphScene(graph);
+    scene = GraphSceneManager::getInstance()->scene(graph);
     view->setScene(scene);
     view->setRenderHints(QPainter::Antialiasing);
     layout = new QVBoxLayout();
