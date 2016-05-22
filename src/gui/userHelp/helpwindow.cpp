e7b441d23e28288a17c6a73217cefb85a04e1bde
Chris Browne <christopher.browne@heig-vd.ch>
2016-05-22 20:59:12 +0200

    added F1 to open help

--- src/gui/userHelp/helpwindow.cpp
+++ src/gui/userHelp/helpwindow.cpp
@@ -1,10 +1,3 @@
-/*! \brief main help window, acts as controller for all signals/slots
- *
- * \file helpwindow.cpp
- * \author Christopher Browne
- * \date 14.05.2016
- */
-
 #include "helpwindow.h"
 
 HelpWindow *HelpWindow::instance = nullptr;
@@ -12,37 +5,11 @@ QString *HelpWindow::_baseUrl = nullptr;
 
 HelpWindow::HelpWindow(QWidget *parent) : parent(parent)
 {
-    leftPane = NavigationPane::getInstance(parent, _baseUrl);
-    rightPane = BrowserPane::getInstance(parent, _baseUrl);
-
-    addLayout(leftPane);
-    addLayout(rightPane);
-
-    QObject::connect(leftPane->getButtonPane()->getLeft(),
-                     SIGNAL(clicked(bool)),
-                     rightPane->getMainBrowser(),
-                     SLOT(backward()));
-
-    QObject::connect(leftPane->getButtonPane()->getRight(),
-                     SIGNAL(clicked(bool)),
-                     rightPane->getMainBrowser(),
-                     SLOT(forward()));
-
-    QObject::connect(rightPane->getSearchBar(),
-                     SIGNAL(returnPressed()),
-                     rightPane,
-                     SLOT(searchAsked()));
-
-    QObject::connect(rightPane,
-                     SIGNAL(searchResultsRequested(QString)),
-                     rightPane->getMainBrowser(),
-                     SLOT(searchAsked(QString)));
-
-    QObject::connect(leftPane->getNavMenu(),
-                     SIGNAL(anchorClicked(QUrl)),
-                     rightPane->getMainBrowser(),
-                     SLOT(setSource(QUrl)));
-
+    setMinimumSize(800,600);
+    setStyleSheet("background-color:white");
+    setWindowTitle("User Guide");
+    setLayout(HelpMainLayout::getInstance(parent, _baseUrl));
+    show();
 }
 
 HelpWindow *HelpWindow::getInstance(QWidget *parent, QString *baseUrl)
@@ -53,3 +20,5 @@ HelpWindow *HelpWindow::getInstance(QWidget *parent, QString *baseUrl)
     }
     return instance;
 }
+
+
