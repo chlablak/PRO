9a96e73c7b315c78e0ed3616809323ab78c77237
Sébastien Richoz <sebastien.richoz1@heig-vd.ch>
2016-05-25 19:24:10 +0200

    BF et Dijkstra pour graphe non orienté

--- src/graph/algorithms/DijkstraSP.cpp
+++ src/graph/algorithms/DijkstraSP.cpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <stdexcept>
 #include "DijkstraSP.h"
+#include "../graphs/Graph.h"
 #include "../graphs/DiGraph.h"
 #include "../graphs/FlowGraph.h"
 
@@ -34,17 +35,52 @@ void DijkstraSP::relax(IEdge *ie) {
 }
 
 void DijkstraSP::visit(Graph *g, Vertex *from) {
-    UNUSED(g);
-    UNUSED(from);
-    throw std::runtime_error("Error with DijkstraSP algorithm. A non directed"
-                                     "graph can't be apply to this algorithm."
-                                     "The graph should be directed");
+    if (!g->isWeighted() || g->isNegativeWeighted()) {
+        throw std::runtime_error("Error in Dijkstra algorithm. The graph must"
+                                         " be positively weighted");
+    }
+
+    Graph *gClone = g->clone();
+    _G = gClone->emptyClone();
+
+    // Init _distances and _edges
+    _distanceTo.assign(g->V(),std::numeric_limits<double>::max());
+    _edgeTo.resize(g->V());
+    _marques.assign(g->V(), false);
+
+    // Search from vertex
+    Vertex *fromCpy;
+    for (Vertex *v : gClone->vertexList()) {
+        if (*v == *from) {
+            fromCpy = v;
+        }
+    }
+
+    _distanceTo[fromCpy->id()] = 0;
+    _edgeTo[fromCpy->id()] = new Edge(fromCpy, fromCpy, 0);
+
+    // Initialization. Insert all vertices of the graph in the priority queue
+    gClone->forEachVertex([this](Vertex *v){
+        _pq.insert( std::make_pair(_distanceTo[v->id()], v) );
+    });
+
+    // Get the min vertex, then treat his adjacent edges to progressively find
+    // the shortest path
+    while(!_pq.empty()){
+        Vertex *u = _pq.begin()->second;
+        _pq.erase(_pq.begin());
+        _marques[u->id()] = true;
+
+        gClone->forEachAdjacentEdge(u, [this](IEdge *ie){
+            relax(ie);
+        });
+    }
 }
 
 void DijkstraSP::visit(DiGraph *g, Vertex *from) {
     if (!g->isWeighted() || g->isNegativeWeighted()) {
         throw std::runtime_error("Error in Dijkstra algorithm. The graph must"
-                                         "be positively weighted");
+                                         " be positively weighted");
     }
 
     DiGraph *gClone = g->clone();
