11db6c1801e4ce403bc7ee6287db6bd741824fc4
Alain Hardy <alain.hardy@heig-vd.ch>
2016-05-29 17:11:48 +0200

    ajout de commentaires

--- src/gui/console.h
+++ src/gui/console.h
@@ -1,3 +1,10 @@
+/*! \brief Implementation of a console re-implementing QTextEdit
+ *
+ * \file console.h
+ * \author Alain Hardy
+ * \date 30.04.2016
+ */
+
 #ifndef CONSOLE_H
 #define CONSOLE_H
 
@@ -20,11 +27,35 @@ class Console : public QTextEdit
 {
     Q_OBJECT
 public:
+    /*!
+     * \brief Default constructor
+     */
     Console();
-    Console(const QString&, QWidget *parent = 0);
+
+    /*!
+     * \brief Construct a console
+     * \param prompt: Specify the value which be displayed in the prompt
+     * \param parent: MainWindow considered as a parent. It is not recommanded to
+     * use another type of widget.
+     */
+    Console(const QString& prompt, QWidget *parent = 0);
+
+    /*!
+     * \brief saveChanges
+     */
     void saveChanges();
 
+    /*!
+     * \brief Prepare and store data of the console into a QByteArray
+     * \return A QByteArray containing the data of the console.
+     */
     QByteArray prepareDataForSave();
+
+    /*!
+     * \brief loadDataToConsole
+     * \param data: QByteArray containing the data to use to restore the console
+     * \param ignoreFilename: Ignore the name of the file when reading the data-
+     */
     void loadDataToConsole(QByteArray& data, bool ignoreFilename = true);
 
     /*!
@@ -55,6 +86,9 @@ protected:
     void focusInEvent(QFocusEvent *e);
 
 private:
+    /*!
+     * \brief Contains the current text
+     */
     QString buffer;    
     QString prompt;
     QLinkedList<QString> commandHistory;
@@ -65,15 +99,31 @@ private:
     static bool interfaced;
     egli::Data dataState;
 
+    /*!
+     * \brief Execute
+     * \param buffer
+     */
     void execute(const QString& buffer);
 
 
     bool hasChanged;
     size_t cursorPosition;
     QTextCursor cursor;
-    const QString fileDelimiter = "°§§°";
 
-    void clearDisplay();
+    /*!
+     * \brief Use to separate commands in console file.
+     */
+    const QString commandDelimiter = "°§§°";
+
+    /*!
+     * \brief Clear the command type by the user
+     */
+    void clearCommand();
+
+    /*!
+     * \brief Fonction that emit a signal that the console has changed,
+     * if the console was not already in "has changed" state.
+     */
     void consoleHasChanged();
 
     static bool drawGraph(const IGraph* graph);
@@ -94,19 +144,57 @@ private:
     QCompleter *completer;
     QStringList *completerWordList;
 
+    /*!
+     * \brief Save the console if it has changed
+     */
     void save();
 
 signals:
+    /*!
+     * \brief Signal changes to parent to make it change the tab
+     */
     void signalChanges();
+
+    /*!
+     * \brief Signal save to parent to make it change the tab
+     */
     void signalSave();
+
+    /*!
+     * \brief Ask the parent what is the name of the tab bar
+     */
     void askTabName(QString&);
+
+    /*!
+     * \brief Ask the parent to change the name of the tab bar
+     */
     void requestTabNameChanges(const QString&);
+
+    /*!
+     * \brief Ask the parent to change the current tab based on the shift
+     * \param shift: A minus value indicate a shift to the left while a plus value a shift to the right
+     */
     void requestTabChange(int shift);
 
 public slots:
+    /*!
+     * \brief Copy the selected text to the clipboard
+     */
     void copySelectedText();
+
+    /*!
+     * \brief Paste text from the clipboard into the console
+     */
     void pasteText();
+
+    /*!
+     * \brief Save console's data into a file
+     */
     void saveConsole();
+
+    /*!
+     * \brief Load console's data from a file
+     */
     void loadConsole();
 
 private slots:
