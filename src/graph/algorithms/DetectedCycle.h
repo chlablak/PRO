406d12b940f90bc2c31fca1341acb8099c3cd1f8
Patrick Djomo <patrick.djomo@heig-vd.ch>
2016-05-30 02:55:43 +0200

    clotûrer avec les commentaires

--- src/graph/algorithms/DetectedCycle.h
+++ src/graph/algorithms/DetectedCycle.h
@@ -1,7 +1,8 @@
-//
-// Created by PatrickDesle on 23.05.2016.
-//
-// TODO Djomo commentaires + QT coding style
+/*! brief Determine weither a graph is acyclique
+ * \file DetectedCycle.h
+ * \author Sébastien Richoz & Patrick Djomo
+ * \date spring 2016
+ */
 
 #ifndef GRAPH_DETECTEDCYCLE_H
 #define GRAPH_DETECTEDCYCLE_H
